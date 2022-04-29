0c12b50526fb623057098d3a57969453bd0241fc
Julio <julioisai.marulandam@um.es>
2022-04-29 17:59:59 +0200

    Estados-v0.1

---
+++ IADJ/Assets/IA/Estados/Default.cs
@@ -0,0 +1,30 @@
+using System.Numerics;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Default : State
+{
+
+
+  
+    public override void GetAction(NPC npc){
+        
+    }
+
+    public override void GetEntryAction(NPC npc){
+
+    }
+
+    public override void GetExitAction(NPC npc){
+
+    }
+
+    public override void CheckState(NPC npc){
+
+    }
+
+}
+
+   
---
+++ IADJ/Assets/IA/NPC.cs
@@ -0,0 +1,70 @@
+using System.Numerics;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NPC : MonoBehaviour
+{
+
+    // Estados posibles del NPC   
+    public Capture stateCapture;
+    public Dead stateDead;
+    public Healing stateHealing;
+    public MeleeAttack stateMeleeAtack;
+    public RangeAttack stateRangeAttack;
+
+    // CARACTERÍSTICAS DEL NPC
+
+    // Puntos de vida Máximos
+    private int _phMax;
+    // Puntos de vida
+    private int _ph;
+    // Puntos de ataque
+    private int _pAttack;
+    // Rango de ataque
+    private int _range;
+    // Velocidad de ataque
+    private int _attackSpeed;
+    // Precisión de ataque
+    private int _hitRate;
+    
+
+    // Movimiento del personaje
+    private AgentNPC _agentNPC;
+    private PathFinding pathFinding;
+
+    // Estado actual del NPC
+    private State _currentState;
+
+
+
+
+
+    void Update(){
+
+    }
+
+
+    public void ChangeState(State newState){
+
+    }
+
+    // Getters y Setters;
+
+    public int PhMax
+    {
+        set { _phMax = value; }
+        get { return _phMax; }
+    }
+
+    public State CurrentState
+    {
+        set { _currentState = value; }
+        get { return _currentState; }
+    }
+
+
+}
+
+   
